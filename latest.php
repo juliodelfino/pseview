6377ca1608ec89062e56d1c156891634f6307712
juliodelfino <julius.delfino@yahoo.com>
2017-05-08 04:08:16 +0800

    Added checking whether the latest.json is outdated by day or not

--- latest.php
+++ latest.php
@@ -1,62 +1,69 @@
- <?php 
-    $dataFile = 'gs://pseview.appspot.com/data/latest.json';
-    
-    if (is_market_open() || is_file_time_during_market_open($dataFile)) {
-        
-        $referer_url = 'http://pse.com.ph/stockMarket/home.html';
-        $get_stocks_url = 'http://pse.com.ph/stockMarket/home.html?method=getSecuritiesAndIndicesForPublic&ajax=true';
-        $opts = array(
-	  'http'=>array(
-	    'method'=>"GET",
-	    'header'=>"Referer: $referer_url\r\n"
-	  )
-	);
+<?php
+$dataFile = 'gs://pseview.appspot.com/data/latest.json';
 
-        $context = stream_context_create($opts);
+if (is_market_open() || is_file_time_during_market_open($dataFile) || is_file_outdated($dataFile)) {
 
-        $text = file_get_contents($get_stocks_url, false, $context);
-               
-        if (empty($text)) {
-           // $text = file_get_contents($dataFile) or die('File not found');
-        } else {
-            file_put_contents($dataFile, $text) or die('Unable to write to a file');
-        }
-    } else {
+    $referer_url = 'http://pse.com.ph/stockMarket/home.html';
+    $get_stocks_url = 'http://pse.com.ph/stockMarket/home.html?method=getSecuritiesAndIndicesForPublic&ajax=true';
+    $opts = array('http' => array('method' => "GET", 'header' => "Referer: $referer_url\r\n"));
+
+    $context = stream_context_create($opts);
+
+    $text = file_get_contents($get_stocks_url, false, $context);
+
+    if (empty($text)) {
         $text = file_get_contents($dataFile) or die('File not found');
+    } else {
+        file_put_contents($dataFile, $text) or die('Unable to write to a file');
     }
-    
-    echo $text;
-    
-    function is_market_open_on_timestamp($timestamp) {
-    
-        $tz = new DateTimeZone('Asia/Singapore');
-        $date = new DateTime();
-        if (!empty($timestamp)) {
-            $date->setTimestamp($timestamp);
-        }
-        $date->setTimezone($tz);
-        $day_of_week = $date->format('w'); //0 for Sunday -> 6 for Saturday
-        $is_open = false;
-        if ($day_of_week > 0 && $day_of_week < 6) {
-            $hr_min = $date->format('Gi');
-            
-            $is_open = ($hr_min >=930 && $hr_min <=1530);
-        }
-        return $is_open; 
+} else {
+    $text = file_get_contents($dataFile) or die('File not found');
+}
+
+echo $text;
+
+function get_DateTime($timestamp) {
+    $tz = new DateTimeZone('Asia/Singapore');
+    $date = new DateTime();
+    if (!empty($timestamp)) {
+        $date -> setTimestamp($timestamp);
     }
-     
-    
-    function is_market_open() {
-    
-        $ret = is_market_open_on_timestamp(time());
-        return $ret;
+    $date -> setTimezone($tz); 
+    return $date;
+}
+
+function is_market_open_on_timestamp($timestamp) {
+
+    $date = get_DateTime($timestamp);
+    $day_of_week = $date -> format('w');
+    //0 for Sunday -> 6 for Saturday
+    $is_open = false;
+    if ($day_of_week > 0 && $day_of_week < 6) {
+        $hr_min = $date -> format('Gi');
+
+        $is_open = ($hr_min >= 930 && $hr_min <= 1530);
     }
+    return $is_open;
+}
+
+function is_market_open() {
+
+    $ret = is_market_open_on_timestamp(time());
+    return $ret;
+}
+
+function is_file_time_during_market_open($filename) {
+
+    $ret = is_market_open_on_timestamp(filemtime($filename));
+    return $ret;
+}
+
+/* Checks if the file is outdated by date YYYY-mm-dd */
+function is_file_outdated($filename) {
     
-    function is_file_time_during_market_open($filename) {
-    
-        $ret = is_market_open_on_timestamp(filemtime($filename));
-        return $ret;
-    }
-    
-    
- ?> 
+    $fileYmd = get_DateTime($timestamp) -> format('Ymd');
+    $nowYmd = get_DateTime(time()) -> format('Ymd');
+    return $nowYmd > $fileYmd;
+}
+
+?>
